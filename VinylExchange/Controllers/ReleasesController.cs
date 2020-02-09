84c9f7a225beee47e6bd22d3cfd19c68b9c8e827
HeLted <44501003+HeLted@users.noreply.github.com>
2020-02-09 23:13:02 +0200

    Set up template for return functionnality in ReleasesController and some cleanup of unused controllers from web api template

--- VinylExchange/Controllers/ReleasesController.cs
+++ VinylExchange/Controllers/ReleasesController.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using VinylExchange.Models.InputModels.Releases;
-using VinylExchange.Services;
 using VinylExchange.Services.MainServices;
 
 namespace VinylExchange.Controllers
@@ -27,24 +23,27 @@ namespace VinylExchange.Controllers
         public async Task<IActionResult> GetReleases(string searchTerm,int releasesToSkip)
         {
             var releases = await this.releasesService.GetReleases(searchTerm, releasesToSkip);
-        
+
+            if (releases == null)
+            {
+                return NotFound();
+            }
+
             return Ok(releases);
         }
 
         [HttpPost]
         [Route("AddRelease")]
-        public async Task<ActionResult> AddRelease(AddReleaseInputModel inputModel)
+        public async Task<IActionResult> AddRelease(AddReleaseInputModel inputModel)
         {
             var release = await releasesService.AddRelease(inputModel);
-
-            var returnObject = new
+            
+            if(release == null)
             {
-                message = "Sucesfully Added Release",
-                id = release.Id
-
-            };
-
-            return Json(returnObject);
+                return BadRequest();
+            }
+                       
+            return CreatedAtRoute($"{release.Id}",release);
         }
 
     }
