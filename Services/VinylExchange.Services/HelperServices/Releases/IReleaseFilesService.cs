3859f80b97904950b745c977b226646ba144169f
HeLted <44501003+HeLted@users.noreply.github.com>
2020-03-06 14:46:04 +0200

    -

---
+++ Services/VinylExchange.Services/HelperServices/Releases/IReleaseFilesService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using VinylExchange.Data.Models;
+using VinylExchange.Models.ResourceModels.ReleaseFiles;
+
+namespace VinylExchange.Services.HelperServices.Releases
+{
+    public interface IReleaseFilesService
+    {
+        Task<IEnumerable<ReleaseFile>> AddFilesForRelease(Guid releaseId, Guid formSessionId);
+        Task<IEnumerable<ReleaseFileResourceModel>> GetReleaseTracks(Guid releaseId);
+        Task<IEnumerable<ReleaseFileResourceModel>> GetReleaseImages(Guid releaseId);
+        Task<ReleaseFileResourceModel> GetReleaseCoverArt(Guid releaseId);
+
+    }
+}
