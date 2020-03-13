ac075a420b8cfeeb1665b67b3992ff7b81ccf4d8
HeLted <44501003+HeLted@users.noreply.github.com>
2020-03-14 01:11:57 +0200

    visual improovements to collection page

--- Web/VinylExchange.Web/ClientApp/src/components/user/collection/CollectionComponent.js
+++ Web/VinylExchange.Web/ClientApp/src/components/user/collection/CollectionComponent.js
@@ -3,44 +3,41 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight, faTimes } from "@fortawesome/free-solid-svg-icons";
 import PlayerLoaderButton from "./../../common/PlayerLoaderButton";
 import GoToReleaseButton from "./../../common/GoToReleaseButton";
-import AddSalePopupFormContainer from "./../../common/popupForms/addSalePopupForm/AddSalePopupContainer";
 import "./Collection.css";
 import AddSalePopupContainer from "./../../common/popupForms/addSalePopupForm/AddSalePopupContainer";
+import GradeBadge from "./../../common/badges/GradeBadge";
 
 function CollectionCoponent(props) {
   return (
-    <div className="col-4">
-      <div className="row border text-center justify-content-center" style={{padding:"30px",height:"500px"}}>
-      
-        <img
-          className="card-img-top"
-          src={props.data.coverArt}
-          alt="Card image cap"
-        />
-        <div >
-       <br/>
-          <div className="col-12 border">
-          <h6>
-              Vinyl:
-              <span className="grade-badge badge badge-primary">
-                {props.data.vinylGrade}
-              </span>
-            </h6>
-            </div>
-            <br/>
-            <div className="col-12 border">
-            <h6>
-      
-             
-              Sleeve:
-              <span className="grade-badge badge badge-primary">
-                {props.data.sleeveGrade}
-              </span>
-            </h6>
-            </div>
-         <br/>
+    <div className="col-lg-4 col-md-6 col-sm-8 col-xs-12">
+      <div
+        className="row border text-center justify-content-center"
+        style={{ padding: "30px", height: "600px" }}
+      >
+         <div className="col-12 ">
+        <img class="img-thumbnail" src={props.data.coverArt} height="150px" width="150px" />
+        </div>
+        <div>
+          <br />
+          <div className="col-12 border mb-10">
+            <h6>Vinyl:</h6>
+          </div>
+          <div className="col-12 mb-10">
+            <GradeBadge data={{ grade: props.data.vinylGrade }} />
+          </div>
+          <div className="col-12 border mb-10">
+            <h6>Sleeve:</h6>
+          </div>
+          <div className="col-12 mb-10">
+            <GradeBadge data={{ grade: props.data.sleeveGrade }} />
+          </div>
+
           <div className="col-12 text-center">
-            <div class="btn-group" role="group" aria-label="Basic example"></div>
+            <div
+              class="btn-group"
+              role="group"
+              aria-label="Basic example"
+            ></div>
             <button
               type="button"
               className="btn-spr btn btn-danger"
@@ -55,18 +52,16 @@ function CollectionCoponent(props) {
             <AddSalePopupContainer
               data={{
                 collectionItemId: props.data.id,
-                releaseId : props.data.releaseId,
+                releaseId: props.data.releaseId,
                 vinylGrade: props.data.vinylGrade,
                 sleeveGrade: props.data.sleeveGrade,
                 description: props.data.description
               }}
             />
             <GoToReleaseButton data={{ releaseId: props.data.releaseId }} />
-          
           </div>
         </div>
       </div>
-   
     </div>
   );
 }
