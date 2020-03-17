e1d664b3cc20c8c4a915d12d19061935012e3849
HeLted <44501003+HeLted@users.noreply.github.com>
2020-03-17 10:51:52 +0200

    -

--- Web/VinylExchange.Web/ClientApp/src/components/user/profile/userModals/confirmEmail/ConfirmEmailModalComponent.js
+++ Web/VinylExchange.Web/ClientApp/src/components/user/profile/userModals/confirmEmail/ConfirmEmailModalComponent.js
@@ -1,4 +1,4 @@
-import Reactfrom "react";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSync } from "@fortawesome/free-solid-svg-icons";
 
