acc75f452894428f768d79e40b3a2ad69ce0a77c
HeLted <44501003+HeLted@users.noreply.github.com>
2020-04-08 23:59:37 +0300

    Added InvalidSaleActionException

---
+++ Services/VinylExchange.Services.Data/MainServices/Sales/Exceptions/InvalidSaleActionException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VinylExchange.Services.Data.MainServices.Sales.Exceptions
+{
+    public class InvalidSaleActionException : Exception
+    {
+        public InvalidSaleActionException(string message) : base (message)
+        {
+
+        }
+    }
+}
