f5ac8a0811447c2cd695c1388c42c00c704ee359
HeLted <44501003+HeLted@users.noreply.github.com>
2020-04-15 15:07:52 +0300

    Added seeding data for releases and its files

--- Data/VinylExchange.Data/Seeding/ReleaseFilesSeeder.cs
+++ Data/VinylExchange.Data/Seeding/ReleaseFilesSeeder.cs
@@ -12,7 +12,7 @@ namespace VinylExchange.Data.Seeding
     public class ReleaseFilesSeeder : ISeeder
     {
         public const string ImagesPath = @"\Image";
-        
+
         public const string AudioFilesPath = @"\Audio";
 
         public async Task SeedAsync(VinylExchangeDbContext dbContext, IServiceProvider serviceProvider)
@@ -20,26 +20,47 @@ namespace VinylExchange.Data.Seeding
 
             if (!dbContext.ReleaseFiles.Any())
             {
-                 //Aphex Twin - Selected Ambient Works 85 - 92
-            await SeedReleaseFileAsync(dbContext,Guid.Parse("3cc8ee5c-4dc3-4009-b3fc-d768e6a564f8"),FileType.Image,
-                "e10ccdba-7f17-4831-a9e9-335a3f2ec96c@---@ZG93bmxvYWQ=.jpg",true);
+                //Aphex Twin - Selected Ambient Works 85 - 92
+                await SeedReleaseFileAsync(dbContext, Guid.Parse("3cc8ee5c-4dc3-4009-b3fc-d768e6a564f8"), FileType.Image,
+                    "e10ccdba-7f17-4831-a9e9-335a3f2ec96c@---@ZG93bmxvYWQ=.jpg", true);
 
-              await SeedReleaseFileAsync(dbContext,Guid.Parse("3cc8ee5c-4dc3-4009-b3fc-d768e6a564f8"),FileType.Audio,
-                "0d8f30d3-67a7-48b6-a2ad-f69d0afebed8@---@QXBoZXggVHdpbiAtIEFnZWlzcG9saXM=.mp3",false);
+                await SeedReleaseFileAsync(dbContext, Guid.Parse("3cc8ee5c-4dc3-4009-b3fc-d768e6a564f8"), FileType.Audio,
+                  "0d8f30d3-67a7-48b6-a2ad-f69d0afebed8@---@QXBoZXggVHdpbiAtIEFnZWlzcG9saXM=.mp3", false);
+
+                await SeedReleaseFileAsync(dbContext, Guid.Parse("3cc8ee5c-4dc3-4009-b3fc-d768e6a564f8"), FileType.Audio,
+             "863aad8d-a80a-453d-b9c8-010e4d84c564@---@QXBoZXggVHdpbiAtIFh0YWw=.mp3", false);
+
+                //De Lacy - Hideaway
+                await SeedReleaseFileAsync(dbContext, Guid.Parse("1cf99ed0-a565-4d2a-928b-99a0fd851d9b"), FileType.Image,
+                  "0002affb-a428-4279-bfc0-5202a1558850@---@Ui00MzgwMS0xMjM5MDM4Nzg2LmpwZWc=.jpg", true);
+
+                await SeedReleaseFileAsync(dbContext, Guid.Parse("1cf99ed0-a565-4d2a-928b-99a0fd851d9b"), FileType.Audio,
+               "de61302f-01a7-446f-9d15-651ab4784d82@---@RGUgJ0xhY3kgSGlkZWF3YXkgW0RlZXAgRGlzaCBNaXhd.mp3", false);
+
+                //Sasha - Wavy Gravy 
+                //22d663f8-6bd6-4f20-9f74-bd82da066e42
+
+              await SeedReleaseFileAsync(dbContext, Guid.Parse("22d663f8-6bd6-4f20-9f74-bd82da066e42"), FileType.Image,
+                  "210d3e68-f74d-45f1-9488-8890b58503fd@---@Ui0yMDg3Nzc1LTEzMzc3NjY3NzktNjg3OS5qcGVn.jpg", true);
+
+                   await SeedReleaseFileAsync(dbContext, Guid.Parse("22d663f8-6bd6-4f20-9f74-bd82da066e42"), FileType.Audio,
+               "129606d8-f943-4fbb-a21b-928af0252490@---@U2FzaGEgLSBXYXZ5IEdyYXZ5.mp3", false);
+             
+
 
             }
 
         }
 
-        private static async Task SeedReleaseFileAsync(VinylExchangeDbContext dbContext, Guid releaseId,FileType fileType,string fileName,bool isPrewiew)
+        private static async Task SeedReleaseFileAsync(VinylExchangeDbContext dbContext, Guid releaseId, FileType fileType, string fileName, bool isPrewiew)
         {
             var releaseFile = new ReleaseFile
             {
-                 ReleaseId = releaseId,
-                  Path = @"\Releases\" + fileType.ToString() + "\\",
-                  FileName = fileName,
-                  IsPreview = isPrewiew,
-                  FileType = fileType
+                ReleaseId = releaseId,
+                Path = @"\Releases\" + fileType.ToString() + "\\",
+                FileName = fileName,
+                IsPreview = isPrewiew,
+                FileType = fileType
             };
 
             await dbContext.ReleaseFiles.AddAsync(releaseFile);
--- Data/VinylExchange.Data/Seeding/ReleasesSeeder.cs
+++ Data/VinylExchange.Data/Seeding/ReleasesSeeder.cs
@@ -14,7 +14,9 @@ namespace VinylExchange.Data.Seeding
         {
             if (!dbContext.Releases.Any())
             {
-                           await SeedReleaseAsync(dbContext,Guid.Parse("3cc8ee5c-4dc3-4009-b3fc-d768e6a564f8"),"Aphex Twin","Selected Ambient Works 85-92","4x Lp",1992,"Warp");
+                await SeedReleaseAsync(dbContext, Guid.Parse("3cc8ee5c-4dc3-4009-b3fc-d768e6a564f8"), "Aphex Twin", "Selected Ambient Works 85-92", "4x Lp", 1992, "Warp");
+                await SeedReleaseAsync(dbContext, Guid.Parse("1cf99ed0-a565-4d2a-928b-99a0fd851d9b"), "De Lacy", "Hideaway", "Vinyl 12'", 1995, "Deconstruction");
+                  await SeedReleaseAsync(dbContext, Guid.Parse("22d663f8-6bd6-4f20-9f74-bd82da066e42"), "Sasha", "Wavy Gravy", "Vinyl 12'", 2002, "BMG UK & Ireland Ltd");
             }
 
         }
